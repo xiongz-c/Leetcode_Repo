381e7dba11fa78bd2afaef3277a00d316ab1fe87
zc-BEAR <xiongzc2018@gmail.com>
2020-06-09 08:39:34 +0800

    modify P0084,P0154

--- src/P0154.java
+++ src/P0154.java
@@ -16,30 +16,40 @@ public class P0154 {
     }
 
     static class Solution {
-        public int findPivot( int[] arr , int begin , int end ) {
-            if ( begin > end ) return -1;
-            if ( begin == end ) return begin;
-            int mid = begin + ( ( end - begin ) >>> 1 );
-            if ( mid < end && arr[mid] > arr[mid + 1] ) return mid;
-            if ( mid > begin && arr[mid - 1] > arr[mid] ) return mid - 1;
-            if ( arr[begin] < arr[mid] ) return findPivot( arr , mid + 1 , end );
-            else return findPivot( arr , begin , mid - 1 );
-        }
-
-        public int findMin( int[] nums ) {
-            List<Integer> list = new ArrayList<>( );
-            for (int i = 0; i < nums.length; i++) {
-                if ( i == 0 ) list.add( nums[i] );
-                else if ( nums[i - 1] != nums[i] ) list.add( nums[i] );
+//        public int findPivot( int[] arr , int begin , int end ) {
+//            if ( begin > end ) return -1;
+//            if ( begin == end ) return begin;
+//            int mid = begin + ( ( end - begin ) >>> 1 );
+//            if ( mid < end && arr[mid] > arr[mid + 1] ) return mid;
+//            if ( mid > begin && arr[mid - 1] > arr[mid] ) return mid - 1;
+//            if ( arr[begin] < arr[mid] ) return findPivot( arr , mid + 1 , end );
+//            else return findPivot( arr , begin , mid - 1 );
+//        }
+//
+//        public int findMin( int[] nums ) {
+//            List<Integer> list = new ArrayList<>( );
+//            for (int i = 0; i < nums.length; i++) {
+//                if ( i == 0 ) list.add( nums[i] );
+//                else if ( nums[i - 1] != nums[i] ) list.add( nums[i] );
+//            }
+//            int[] arr = new int[list.size( )];
+//            for (int i = 0;i < list.size();i++){
+//                arr[i] = list.get( i );
+//            }
+//            int pivot = findPivot( arr,0,arr.length - 1);
+//            if(pivot==-1)return arr[0];
+//            else if(pivot!=arr.length-1)return arr[pivot+1];
+//            else return arr[0];
+//        }
+        public int findMin(int[] nums){
+            int lo = 0, hi = nums.length - 1;
+            while(lo < hi){
+                int mid = (lo+hi) >>> 1;
+                if(nums[mid] > nums[hi]) lo = mid +1;
+                else if(nums[mid] < nums[hi]) hi = mid;
+                else hi--;
             }
-            int[] arr = new int[list.size( )];
-            for (int i = 0;i < list.size();i++){
-                arr[i] = list.get( i );
-            }
-            int pivot = findPivot( arr,0,arr.length - 1);
-            if(pivot==-1)return arr[0];
-            else if(pivot!=arr.length-1)return arr[pivot+1];
-            else return arr[0];
+            return nums[lo];
         }
     }
 
